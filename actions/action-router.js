e135351b389a08281147eccfd5d078d5ef3bb978
cpscott1 <thacam18@gmail.com>
2019-05-18 12:48:54 -0500

    made initial endpoints

--- actions/action-router.js
+++ actions/action-router.js
@@ -3,3 +3,27 @@ const express = require('express');
 const router = express.Router();
 
 const actionDb = require('../data/helpers/actionModel.js');
+
+router.use(express.json())
+
+router.get('/', (req, res) => {
+
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router;
--- projects/project-router.js
+++ projects/project-router.js
@@ -3,3 +3,27 @@ const express = require('express');
 const router = express.Router();
 
 const projectDb = require('../data/helpers/projectModel.js');
+
+router.use(express.json())
+
+router.get('/', (req, res) => {
+
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router;
