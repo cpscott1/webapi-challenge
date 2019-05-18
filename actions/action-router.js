25659ea03dc3b23fae7cbff5ce25d18509c2ffba
cpscott1 <thacam18@gmail.com>
2019-05-18 12:32:33 -0500

    setup routes

---
+++ actions/action-router.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const router = express.Router();
+
+const actionDb = require('../data/helpers/actionModel.js');
---
+++ projects/project-router.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const router = express.Router();
+
+const projectDb = require('../data/helpers/projectModel.js');
